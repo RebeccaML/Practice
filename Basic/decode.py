110bcaf7c5628fc02cd3cd336be32e8529bd9190
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2019-05-12 15:25:44 -0700

    Added comments

--- Basic/decode.py
+++ Basic/decode.py
@@ -6,14 +6,19 @@ code = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmg
 translation = ""
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
+#Cycle through letters in code string
 for letter in code:
+    #Check for non-alpha chars such as spaces and add to the translation string
     if letter.isalpha() == False:
         translation += letter
     else:
+        #Cycle through letters in alphabet string using enumerate so that index can be used
         for index, value in enumerate(alphabet):
             if value == letter:
+                #If near the end of the string a special case is needed to avoid exceeding string length
                 if index > 23:
                     translation += alphabet[index-24]
+                #Otherwise just add 2 to the index of the matched alphabet letter to get the letter two places further
                 else:
                     translation += alphabet[index+2]
 

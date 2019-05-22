dd0bfe876edf48946bfb010b2083bbbda098f221
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2019-05-22 13:55:15 -0700

    Fizzbuzz challenge

--- Python/Basic/fizzbuzz.py
+++ Python/Basic/fizzbuzz.py
@@ -1,5 +1,7 @@
-# Practice file
-# Fizzbuzz attempt
+# Fizzbuzz challenge
+# For all numbers from 1 to 100, if number is a multiple of 3 print Fizz
+# if a multiple of 5 print Buzz, if a multiple of both print Fizzbuzz
+# Otherwise print the number
 
 def fizzbuzz():
     for i in range(1, 101):

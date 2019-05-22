e122f1471090bb8b36ee2ece6f93959e5af1f8a7
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2019-05-22 13:25:47 -0700

    I don't know

--- Web/fullstack_template/static/js/App.jsx
+++ Web/fullstack_template/static/js/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Hello from "./Hello";
 
 export default class App extends React.Component {
     render() {
-        return <p>Hello React!</p>
+        return (
+                <div className="header-contents">
+                    <h1><Hello name="Rimini" /></h1>
+                </div>
+        );
     }
 }

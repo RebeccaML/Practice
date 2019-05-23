0124fa38e89112ee9a14cda03ca6b13ef972e77c
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2019-05-23 09:14:18 -0700

    findRareChars challenge added and some commenting on decode

--- Python/Basic/decode.py
+++ Python/Basic/decode.py
@@ -1,24 +1,23 @@
-#Challenge found here http://www.pythonchallenge.com/pc/def/map.html
-#Decode the string by changing each letter to the one two places further in the alphabet
-#Is this the best solution? I don't know yet but it's what I came up with and it works
+# Challenge found here http://www.pythonchallenge.com/pc/def/map.html
+# Decode the string by changing each letter to the one two places further in the alphabet
 
 code = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 translation = ""
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-#Cycle through letters in code string
+# Cycle through letters in code string
 for letter in code:
-    #Check for non-alpha chars such as spaces and add to the translation string
+    # Check for non-alpha chars such as spaces and add to the translation string
     if letter.isalpha() == False:
         translation += letter
     else:
-        #Cycle through letters in alphabet string using enumerate so that index can be used
+        # Cycle through letters in alphabet string using enumerate so that index can be used
         for index, value in enumerate(alphabet):
             if value == letter:
-                #If near the end of the string a special case is needed to avoid exceeding string length
+                # If near the end of the string a special case is needed to avoid exceeding string length
                 if index > 23:
                     translation += alphabet[index-24]
-                #Otherwise just add 2 to the index of the matched alphabet letter to get the letter two places further
+                # Otherwise just add 2 to the index of the matched alphabet letter to get the letter two places further
                 else:
                     translation += alphabet[index+2]
 
---
+++ Python/Basic/findRareChars.py
@@ -0,0 +1,24 @@
+# Challenge found here: http://www.pythonchallenge.com/pc/def/ocr.html
+# Read a text file and look for rare chars
+# Answer is equality
+
+# Opens the file
+file = open("rareChars.txt")
+file_string = file.read()
+uniqueChars = [] # List to contain every unique character in the file
+
+# Look for each unique character and put them in a list
+# I feel I have the answer already at this point, but finished anyway
+for char in file_string:
+    if char not in uniqueChars:
+        uniqueChars.append(char)
+
+# For every character in the uniqueChars list, count how many times is apppears in the file
+# and print before moving to the next unique char
+
+for u_char in uniqueChars:
+    count = 0
+    for char in file_string:
+        if u_char == char:
+            count += 1
+    print(u_char, "appears", count, "times.")

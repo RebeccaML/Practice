8d6be4d754b6e715f5fff7e0564794eaca5be12f
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2019-05-30 21:16:55 -0700

    Codecademy project

---
+++ Python/Basic/hairSalon.py
@@ -0,0 +1,29 @@
+# Sixth project for Learn Python 3 from Codecademy
+
+hairstyles = ["bouffant", "pixie", "dreadlocks",
+              "crew", "bowl", "bob", "mohawk", "flattop"]
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+last_week = [2, 3, 5, 8, 4, 4, 6, 2] # Meant to represent the number of customers that got eat kind of haircut
+
+total_price = 0
+total_revenue = 0
+
+for price in prices:
+    total_price += price
+
+average_price = total_price / len(prices)
+print("Average Haircut Price:", round(average_price, 2))
+
+new_prices = [price - 5 for price in prices]
+print(new_prices)
+
+for i in range(len(hairstyles)):
+    total_revenue += prices[i] * last_week[i]
+
+print("Total Revenue:", total_revenue)
+average_daily_revenue = total_revenue / 7
+print("Average Daily Revenue:", average_daily_revenue)
+
+cuts_under_30 = [hairstyles[i]
+                 for i in range(len(new_prices)) if new_prices[i] < 30]
+print(cuts_under_30)

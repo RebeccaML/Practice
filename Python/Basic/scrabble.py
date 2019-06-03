1bf7967bae5e09fa90e4df6a9e85b664fa127ee9
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2019-06-03 08:35:19 -0700

    formatting

--- Python/Basic/scrabble.py
+++ Python/Basic/scrabble.py
@@ -1,27 +1,32 @@
 # Eighth project for Learn Python 3 from Codecademy
 # Make a scoring system for a scrabble game using from the following two lists
 
-letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+           "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1,
+          3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
 
 # Convert the lists into a dictionary
-letters_to_points = {key:value for key, value in zip(letters, points)}
+letters_to_points = {key: value for key, value in zip(letters, points)}
 
 # Add a blank space key-value pair
 letters_to_points[" "] = 0
 
 # This takes in a word and calculates how many points it is worth
+
+
 def score_word(word):
-  point_total = 0
-  for letter in word:
-    point_total += letters_to_points.get(letter, 0)
-  return point_total
+    point_total = 0
+    for letter in word:
+        point_total += letters_to_points.get(letter, 0)
+    return point_total
+
 
 # Dictionary with keys being players and their values being lists of the words they've played
 player_to_words = {"player1": ["BLUE", "TENNIS", "EXIT"],
                    "wordNerd": ["EARTH", "EYES", "MACHINE"],
-                  "Lexi Con": ["ERASER", "BELLY", "HUSKY"],
-                  "Prof Reader": ["ZAP", "COMA", "PERIOD"]}
+                   "Lexi Con": ["ERASER", "BELLY", "HUSKY"],
+                   "Prof Reader": ["ZAP", "COMA", "PERIOD"]}
 
 # Dictionary that will hold the final scores
 player_to_points = {}
@@ -32,10 +37,10 @@ player_to_points = {}
 # Add that score to the player_points variable
 # Add the player's name as a key and their total points as the value in the player_to_points dictionary
 for player, words in player_to_words.items():
-  player_points = 0
-  for word in words:
-    player_points += score_word(word)
-  player_to_points[player] = player_points
+    player_points = 0
+    for word in words:
+        player_points += score_word(word)
+    player_to_points[player] = player_points
 
 # Print the final scores
 print(player_to_points)

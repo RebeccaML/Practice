632d6f14662882918d98d40da43aa38039a8157c
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2019-06-05 19:56:51 -0700

    bmi calculator added

--- Web/calculator/calculator.js
+++ Web/calculator/calculator.js
@@ -20,6 +20,18 @@ app.post("/", function(req, res) {
     res.send("The result of the calculation is " + result);
 })
 
+app.get("/bmiCalculator", function(req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmiCalculator", function(req, res) {
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+    var result = (weight / (height * height)).toFixed(2);
+
+    res.send("Your BMI is " + result);
+})
+
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 });

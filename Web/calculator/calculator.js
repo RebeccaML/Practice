c00158381d5fd0546a099b565bfb4f00ee49fe0f
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2019-06-05 17:41:20 -0700

    Node.js calculator

---
+++ Web/calculator/calculator.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+// Urlencoded is used specifically for reading info submitted via an HTML form
+// Extended allows us to post 
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Send the html page to the server
+// __dirname represents the current directory
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 + num2;
+    
+    res.send("The result of the calculation is " + result);
+})
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});

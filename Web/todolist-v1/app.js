41a7b2b03a138391cd93219a315ae3cbb1fc601f
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2019-06-08 16:57:40 -0700

    to do list version 1 done

--- Web/todolist-v1/app.js
+++ Web/todolist-v1/app.js
@@ -1,26 +1,55 @@
+// Made as part of the https://www.udemy.com/course/the-complete-web-development-bootcamp/ course.
+// The CSS was provided and I just made a few minor changes
+
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
+
+const items = ["Practice Javascript", "Practice Python", "Make dinner"];
+const workItems = [];
 
 // This is so we can use templates instead of making a zillion static pages
 // Must be set below the previous line, otherwise app won't exist yet
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+// So links for css, images etc work
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    var today = new Date();
-    var currentDay = today.getDay();
-    var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    var day = daysOfWeek[currentDay];
+    day = date.getDate();
+    res.render("list", {
+        listTitle: day,
+        listItems: items
+    });
+});
 
-    res.render("list", {dayType: day});
-//     if (currentDay === 6 || currentDay === 0) {
-//         res.render("list", {dayType: day});
-//     } else {
-//         res.render("list", {dayType: day});
-//     }
+app.post("/", function (req, res) {
+    let item = req.body.newItem;
+    // addItem is the submit button name
+    if (req.body.addItem === "Work List") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+});
+
+app.get("/work", function (req, res) {
+    res.render("list", {
+        listTitle: "Work List",
+        listItems: workItems
+    });
+});
+
+app.get("/about", function(req, res) {
+    res.render("about");
 });
 
 app.listen(3000, function () {
     console.log("Server started on port 3000.")
-})
+});

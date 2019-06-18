d41436f9b4df645549257d7afa1b725e6d12f42d
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2019-06-18 13:28:40 -0700

    regex prac

---
+++ Python/Basic/regex2.py
@@ -0,0 +1,9 @@
+import re
+
+url_regex = re.compile(r"(https?)://(www\.[A-Za-z-]{2,256}\.[a-z]{2,6})([-a-zA-Z0-9@:%_\+.~#?&//=]*)")
+match = url_regex.search("http://www.youtube.com/videos/fgs/gfd/asd")
+print(match.group())
+print(match.groups())
+print(f"Protocol: {match.group(1)}")
+print(f"Domain: {match.group(2)}")
+print(f"Everything Else: {match.group(3)}")

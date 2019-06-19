0d050769b777be3e745c01c8e2bf2f5aa0c49f36
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2019-06-19 16:06:33 -0700

    home page made

--- Python/Django/locallibrary/catalog/views.py
+++ Python/Django/locallibrary/catalog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from catalog.models import Book, Author, BookInstance, Genre, Language
 # Create your views here.
+
+def index(request):
+    """View function for home page of site."""
+
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+    num_genres = Genre.objects.count()
+    num_languages = Language.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_languages': num_languages,
+    }
+
+    return render(request, 'index.html', context=context)

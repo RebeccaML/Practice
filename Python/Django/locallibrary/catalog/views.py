6017d2b997a9c6a6728b3c67f8cdc77a61f356c1
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2019-06-20 15:59:54 -0700

    user page done

--- Python/Django/locallibrary/catalog/views.py
+++ Python/Django/locallibrary/catalog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre, Language
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 def index(request):
@@ -12,6 +13,8 @@ def index(request):
     num_authors = Author.objects.count()
     num_genres = Genre.objects.count()
     num_languages = Language.objects.count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     context = {
         'num_books': num_books,
@@ -20,6 +23,7 @@ def index(request):
         'num_authors': num_authors,
         'num_genres': num_genres,
         'num_languages': num_languages,
+        'num_visits': num_visits,
     }
 
     return render(request, 'index.html', context=context)
@@ -37,3 +41,12 @@ class AuthorListView(generic.ListView):
 
 class AuthorDetailView(generic.DetailView):
     model = Author
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')

4692154e4c1833fca38f830703d4ec23b05a2e3c
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2019-06-20 16:17:28 -0700

    librarian views

--- Python/Django/locallibrary/catalog/views.py
+++ Python/Django/locallibrary/catalog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre, Language
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
 # Create your views here.
 
 def index(request):
@@ -50,3 +51,13 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+class LoanedBooksAllListView(PermissionRequiredMixin, generic.ListView):
+    """Generic class-based view listing all books on loan. Only visible to users with can_mark_returned permission."""
+    model = BookInstance
+    permission_required = 'catalog.can_mark_returned'
+    template_name = 'catalog/bookinstance_list_borrowed_all.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')

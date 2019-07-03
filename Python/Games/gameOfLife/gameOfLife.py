14c0bac3346bae26283c71b62b1f18a6960aa4c4
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2019-07-03 09:06:35 -0700

    odin project javascript exercises

--- Python/Games/gameOfLife/gameOfLife.py
+++ Python/Games/gameOfLife/gameOfLife.py
@@ -28,4 +28,13 @@ def render(state):
     print(" " + "-" * len(state[0]))
 
 
+def next_board_state(state):
+    new_state = dead_state(len(state[0]), len(state))
+    for i in range(len(state)):
+        for j in range(len(state[i])):
+            if j == 0:
+                if i == 0:
+                    # has 3 neighbours
+                else:
+                    # has 5 neighbours
 render(random_state(15, 15))

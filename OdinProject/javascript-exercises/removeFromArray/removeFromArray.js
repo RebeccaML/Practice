5c8cd8db4dc1418aadb8acc6c19c36a107c5d759
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2019-07-03 20:47:39 -0700

    javascript exercises

--- OdinProject/javascript-exercises/removeFromArray/removeFromArray.js
+++ OdinProject/javascript-exercises/removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let toDelete = args;
+    arr = arr.filter(item => !toDelete.includes(item));
+    return arr
 }
 
 module.exports = removeFromArray
--- OdinProject/javascript-exercises/tempConversion/tempConversion.js
+++ OdinProject/javascript-exercises/tempConversion/tempConversion.js
@@ -1,12 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function (f) {
+    result = (f - 32) * 5 / 9;
+    return Math.round(result * 10) / 10;
 }
 
-const ctof = function() {
-
+const ctof = function (c) {
+    result = c * 9 / 5 + 32;
+    return Math.round(result * 10) / 10;
 }
 
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof
 }

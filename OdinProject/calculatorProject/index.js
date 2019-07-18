cc5340765437a699dd9aa1aa79b696c2325c84b8
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2019-07-18 15:14:14 -0700

    refining display

--- OdinProject/calculatorProject/index.js
+++ OdinProject/calculatorProject/index.js
@@ -1,3 +1,15 @@
+function handleCalculation(operator, a, b) {
+    if (operator === "add") {
+        add(a, b);
+    } else if (operator === "subtract") {
+        subtract(a, b);
+    } else if (operator === "multiply") {
+        multiply(a, b);
+    } else if (operator === "divide") {
+        divide(a, b);
+    }
+}
+
 function add(a, b) {
     return a + b;
 }
@@ -14,21 +26,42 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(op, a, b) {
-    return op(a, b);
-}
-
 let display = document.querySelector("#display");
 display.innerHTML = 0;
+let operator = "";
+let num1 = 0;
+let num2 = 0;
+let temp = 0;
+
+function handleInput(input) {
+    if (input === "add") {
+        updateDisplay("+");
+    } else if (input === "subtract") {
+        updateDisplay("-");
+    } else if (input === "multiply") {
+        updateDisplay("×");
+    } else if (input === "divide") {
+        updateDisplay("÷");
+    } else {
+        updateDisplay(input);
+    }
+    calculation.push(input);
+    console.log(calculation);
+    if (calculation.length === 3) {
+        handleCalculation(calculation);
+    }
+    if (input === "clear") {
+        display.innerHTML = 0;
+        calculation = [];
+        num1 = 0;
+        num2 = 0;
+    }
+}
 
 function updateDisplay(input) {
     if (display.innerHTML == 0) {
         display.innerHTML = input;
-    }
-    else if (input === "clear") {
-        display.innerHTML = 0;
-    }
-    else {
+    } else {
         display.innerHTML += input;
     }
 }
@@ -36,6 +69,6 @@ function updateDisplay(input) {
 buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
     button.addEventListener("click", (e) => {
-        updateDisplay(button.id);
+        handleInput(button.id);
     });
 });

08b3d7e53527b2fa403ea50dafc23d42c3112e92
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2019-08-30 16:30:36 -0700

    refactored to classes

--- JavaScript/Bootcamp/OOP/person.js
+++ JavaScript/Bootcamp/OOP/person.js
@@ -1,41 +1,65 @@
-const Person = function (firstName, lastName, age, likes = []) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.likes = likes;
-};
-
-Person.prototype.getBio = function () {
-    let bio = `${this.firstName} is ${this.age}.`
-    this.likes.forEach((like) => {
-        bio = bio + ` ${this.firstName} likes ${like}.`
-    });
-    return bio;
+class Person {
+    constructor(firstName, lastName, age, likes = []) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.likes = likes;
+    }
+    getBio() {
+        let bio = `${this.firstName} is ${this.age}.`
+        this.likes.forEach((like) => {
+            bio = bio + ` ${this.firstName} likes ${like}.`
+        });
+        return bio;
+    }
+    setName(fullName) {
+        const names = fullName.split(" ");
+        this.firstName = names[0];
+        this.lastName = names[1];
+    }
 }
 
-Person.prototype.setName = function(fullName) {
-    const names = fullName.split(" ");
-    this.firstName = names[0];
-    this.lastName = names[1];
-};
+const myPerson = new Person("Dorn", "Il-Khan", 22, ["killing", "blood"]);
+console.log(myPerson.getBio());
 
-const person1 = new Person("Rebecca", "Lowe", 18, ["fighting", "Helm"]);
-person1.setName("Ajantis Ilvastarr");
-console.log(person1);
-console.log(person1.getBio());
+// const Person = function (firstName, lastName, age, likes = []) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.age = age;
+//     this.likes = likes;
+// };
 
-Person.prototype.getBio = function () {
-    return "Testing testing";
-}
+// Person.prototype.getBio = function () {
+//     let bio = `${this.firstName} is ${this.age}.`
+//     this.likes.forEach((like) => {
+//         bio = bio + ` ${this.firstName} likes ${like}.`
+//     });
+//     return bio;
+// }
 
-const person2 = new Person("Silandra", "Candlekeep", 18, ["Sorcery", "Mirrors"]);
-console.log(person2);
-console.log(person2.getBio());
+// Person.prototype.setName = function(fullName) {
+//     const names = fullName.split(" ");
+//     this.firstName = names[0];
+//     this.lastName = names[1];
+// };
 
-const person3 = new Person("Imoen", "Winthrop", 17, ["Magic", "Pink", "Thieving"]);
+// const person1 = new Person("Rebecca", "Lowe", 18, ["fighting", "Helm"]);
+// person1.setName("Ajantis Ilvastarr");
+// console.log(person1);
+// console.log(person1.getBio());
 
-person3.getBio = function () {
-    return "This is fake";
-}
-console.log(person3);
-console.log(person3.getBio());
+// Person.prototype.getBio = function () {
+//     return "Testing testing";
+// }
+
+// const person2 = new Person("Silandra", "Candlekeep", 18, ["Sorcery", "Mirrors"]);
+// console.log(person2);
+// console.log(person2.getBio());
+
+// const person3 = new Person("Imoen", "Winthrop", 17, ["Magic", "Pink", "Thieving"]);
+
+// person3.getBio = function () {
+//     return "This is fake";
+// }
+// console.log(person3);
+// console.log(person3.getBio());
--- JavaScript/Bootcamp/Projects/hangman/hangman.js
+++ JavaScript/Bootcamp/Projects/hangman/hangman.js
@@ -1,64 +1,128 @@
-const Hangman = function (word, remainingGuesses) {
-    this.word = word.toLowerCase().split("");
-    this.remainingGuesses = remainingGuesses;
-    this.guessedLetters = [];
-    this.status = "playing";
-}
-
-Hangman.prototype.getPuzzle = function () {
-    let puzzle = "";
-    this.word.forEach((letter) => {
-        if (this.guessedLetters.includes(letter) || letter == " ") {
-            puzzle += letter;
-        } else {
-            puzzle += "*";
-        }
-    });
-    return puzzle;
-}
-
-Hangman.prototype.makeGuess = function (guess) {
-    guess = guess.toLowerCase();
-    const isUnique = !this.guessedLetters.includes(guess);
-    const isBadGuess = !this.word.includes(guess);
-
-    if (this.status !== "playing") {
-        return;
-    }
-
-    if (isUnique) {
-        this.guessedLetters.push(guess);
-    }
-    if (isUnique && isBadGuess) {
-        this.remainingGuesses--;
-    }
-    this.calculateStatus();
-}
-
-Hangman.prototype.calculateStatus = function () {
-    const lettersUnguessed = this.word.filter((letter) => {
-        return !this.guessedLetters.includes(letter);
-    });
-
-    const finished = lettersUnguessed.length === 0;
-
-    if (this.remainingGuesses === 0) {
-        this.status = "failed";
-    } else if (finished) {
-        this.status = "finished";
-    } else {
+class Hangman {
+    constructor (word, remainingGuesses) {
+        this.word = word.toLowerCase().split("");
+        this.remainingGuesses = remainingGuesses;
+        this.guessedLetters = [];
         this.status = "playing";
     }
+    getPuzzle() {
+        let puzzle = "";
+        this.word.forEach((letter) => {
+            if (this.guessedLetters.includes(letter) || letter == " ") {
+                puzzle += letter;
+            } else {
+                puzzle += "*";
+            }
+        });
+        return puzzle;
+    }
+    makeGuess(guess) {
+        guess = guess.toLowerCase();
+        const isUnique = !this.guessedLetters.includes(guess);
+        const isBadGuess = !this.word.includes(guess);
+    
+        if (this.status !== "playing") {
+            return;
+        }
+    
+        if (isUnique) {
+            this.guessedLetters.push(guess);
+        }
+        if (isUnique && isBadGuess) {
+            this.remainingGuesses--;
+        }
+        this.calculateStatus();
+    }
+    calculateStatus() {
+        const lettersUnguessed = this.word.filter((letter) => {
+            return !this.guessedLetters.includes(letter);
+        });
+    
+        const finished = lettersUnguessed.length === 0;
+    
+        if (this.remainingGuesses === 0) {
+            this.status = "failed";
+        } else if (finished) {
+            this.status = "finished";
+        } else {
+            this.status = "playing";
+        }
+    }
+    getStatusMessage() {
+        if (this.status == "playing") {
+            return `Guesses left: ${this.remainingGuesses}`;
+        }
+        else if (this.status === "failed") {
+            return `Nice try! The word was "${this.word.join("")}".`
+        }
+        else {
+            return "Great work! You guessed the word."
+        }
+    }
 }
 
-Hangman.prototype.getStatusMessage = function () {
-    if (this.status == "playing") {
-        return `Guesses left: ${this.remainingGuesses}`;
-    }
-    else if (this.status === "failed") {
-        return `Nice try! The word was "${this.word.join("")}".`
-    }
-    else {
-        return "Great work! You guessed the word."
-    }
-}
+
+// const Hangman = function (word, remainingGuesses) {
+//     this.word = word.toLowerCase().split("");
+//     this.remainingGuesses = remainingGuesses;
+//     this.guessedLetters = [];
+//     this.status = "playing";
+// }
+
+// Hangman.prototype.getPuzzle = function () {
+//     let puzzle = "";
+//     this.word.forEach((letter) => {
+//         if (this.guessedLetters.includes(letter) || letter == " ") {
+//             puzzle += letter;
+//         } else {
+//             puzzle += "*";
+//         }
+//     });
+//     return puzzle;
+// }
+
+// Hangman.prototype.makeGuess = function (guess) {
+//     guess = guess.toLowerCase();
+//     const isUnique = !this.guessedLetters.includes(guess);
+//     const isBadGuess = !this.word.includes(guess);
+
+//     if (this.status !== "playing") {
+//         return;
+//     }
+
+//     if (isUnique) {
+//         this.guessedLetters.push(guess);
+//     }
+//     if (isUnique && isBadGuess) {
+//         this.remainingGuesses--;
+//     }
+//     this.calculateStatus();
+// }
+
+// Hangman.prototype.calculateStatus = function () {
+//     const lettersUnguessed = this.word.filter((letter) => {
+//         return !this.guessedLetters.includes(letter);
+//     });
+
+//     const finished = lettersUnguessed.length === 0;
+
+//     if (this.remainingGuesses === 0) {
+//         this.status = "failed";
+//     } else if (finished) {
+//         this.status = "finished";
+//     } else {
+//         this.status = "playing";
+//     }
+// }
+
+// Hangman.prototype.getStatusMessage = function () {
+//     if (this.status == "playing") {
+//         return `Guesses left: ${this.remainingGuesses}`;
+//     }
+//     else if (this.status === "failed") {
+//         return `Nice try! The word was "${this.word.join("")}".`
+//     }
+//     else {
+//         return "Great work! You guessed the word."
+//     }
+// }

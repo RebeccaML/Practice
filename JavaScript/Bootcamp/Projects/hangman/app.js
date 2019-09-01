886ca5451058c42b694694101229b2acaae856a9
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2019-09-01 13:04:51 -0700

    closures

--- JavaScript/Bootcamp/Projects/hangman/app.js
+++ JavaScript/Bootcamp/Projects/hangman/app.js
@@ -12,34 +12,22 @@ window.addEventListener("keypress", (e) => {
     guessesElement.textContent = game1.statusMessage;
 });
 
-const request = new XMLHttpRequest();
-
-request.addEventListener("readystatechange", (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText);
-        console.log(data);
+getPuzzle((error, puzzle) => {
+    if (error) {
+        console.log(`Error: ${error}`);
     }
-    else if (e.target.readyState === 4) {
-            console.log("An error has occurred.");
-        }
-    });
-
-request.open("GET", "http://puzzle.mead.io/puzzle?wordCount=3");
-request.send();
-
-const countryCode = "AU";
-const countryRequest = new XMLHttpRequest();
-
-countryRequest.addEventListener("readystatechange", (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText);
-        const country = data.find((country) => country.alpha2Code === countryCode);
-        console.log(country.name);
+    else {
+        console.log(puzzle);
     }
-    else if (e.target.readyState === 4) {
-            console.log("An error has occurred.");
-        }
-    });
+});
 
-countryRequest.open("GET", "http://restcountries.eu/rest/v2/all");
-countryRequest.send();
+console.log("Do something else");
+
+getCountry("FI", (error, countryName) => {
+    if (error) {
+        console.log(`Error: ${error}`);
+    }
+    else {
+        console.log(countryName);
+    }
+});

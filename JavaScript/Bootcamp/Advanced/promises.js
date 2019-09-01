6ca9bee4477da376f57fb44cd5cd8523d9a7632d
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2019-09-01 14:14:37 -0700

    promise chaining

--- JavaScript/Bootcamp/Advanced/promises.js
+++ JavaScript/Bootcamp/Advanced/promises.js
@@ -1,16 +1,25 @@
 //Callback
-const getDataCallback = (callback) => {
+const getDataCallback = (num, callback) => {
     setTimeout(() => {
-        callback("This is my callback error", undefined);
-}, 2000)
+        if (typeof num === "number") {
+            callback(undefined, num * 2);
+        } else {
+            callback("Number must be provided.");
+        }
+    }, 2000)
 };
 
-getDataCallback((error, data) => {
+getDataCallback(2, (error, data) => {
     if (error) {
         console.log(error);
-    }
-    else {
-        console.log(data);
+    } else {
+        getDataCallback(data, (error, data) => {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(data);
+            }
+        });
     }
 });
 
@@ -24,17 +33,26 @@ getDataCallback((error, data) => {
 //     });
 // }
 
-const getDataPromise = (data) => new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(`This is my success data: ${data}`);
-            // reject("This is my promise error.");
-        }, 2000)
-    });
+const getDataPromise = (num) => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        typeof num === "number" ? resolve(num * 2) : reject("Number must be provided.");
+    }, 2000)
+});
 
-const myPromise = getDataPromise(3534545);
-
-myPromise.then((data) => {
-    console.log(data);
+getDataPromise(2).then((data) => {
+    getDataPromise(data).then((data) => {
+        console.log(`Promise data: ${data}.`);
+    }, (error) => {
+        console.log(error);
+    })
 }, (error) => {
+    console.log(error);
+});
+
+getDataPromise(10).then((data) => {
+    return getDataPromise(data);
+}).then((data) => {
+    console.log(data);
+}).catch((error) => {
     console.log(error);
 });

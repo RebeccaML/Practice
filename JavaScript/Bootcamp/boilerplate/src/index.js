108acb6fc168b57274b081589b1955e379d10d6c
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2019-09-06 13:40:01 -0700

    webpack boilerplate

--- JavaScript/Bootcamp/boilerplate/src/index.js
+++
@@ -1,9 +0,0 @@
-// import "./utilities";
-import square, { add, name } from "./utilities";
-import scream from "./scream";
-
-console.log("index.js");
-console.log(add(4, 6));
-console.log(name);
-console.log(scream("hello"));
-console.log(square(3));
--- JavaScript/Bootcamp/boilerplate/src/scream.js
+++
@@ -1,5 +0,0 @@
-console.log("scream.js");
-
-const scream = (inputStr) => inputStr.toUpperCase() + "!";
-
-export default scream;
